28a2e42f3ce6cebdbde5000ac34dbca6cc44b911
Mohit <mohit94596@gmail.com>
2025-02-18 16:06:15 +0530

    Implement: Numbers separated by new lines return their sum

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,7 +2,7 @@ function add(numbers) {
     if (numbers === '') {
         return 0
     }
-    const nums = numbers.split(',').map(Number) //splitting the array based on the delimiter and converting each value to number
+    const nums = numbers.split(/[\n,]/).map(Number) //splitting the array based on the delimiter and converting each value to number
     const sumOfValues = nums.reduce((sum, num) => sum + num, 0) //finding the sum of values, works for any amount of numbers
     return sumOfValues
 }
