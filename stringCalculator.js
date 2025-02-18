37d132de42b76cea65d8292c059a641a7e82a280
Mohit <mohit94596@gmail.com>
2025-02-18 15:53:35 +0530

    Implement: Two numbers separated by comma return their sum

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,7 +2,10 @@ function add(numbers) {
     if (numbers === '') {
         return 0
     }
-    return parseInt(numbers)
+    const nums = numbers.split(',').map(Number) //splitting the array based on the delimiter and converting each value to number
+    const sumOfValues = nums.reduce((sum, num) => sum + num, 0) //finding the sum of values
+    return sumOfValues
 }
+
 
 module.exports = { add }
