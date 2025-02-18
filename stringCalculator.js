41845245963d1028c79c41aa9499f1772f5890af
Mohit <mohit94596@gmail.com>
2025-02-18 17:25:44 +0530

    Implement: Multiple delimiters of varying length supported

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,17 +1,29 @@
 function add(numbers) {
-    if (numbers === '') {
-        return 0
-    }
-    let delimiter = /[,\n]/
-    if (numbers.startsWith('//')){
-        const parts = numbers.split('\n')
-        delimiter = parts[0].substring(2)
+    if (numbers === "") return 0
+
+    let delimiter = /[\n,]/
+
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n")
+        const delimiterPart = parts[0].slice(2)
         numbers = parts[1]
+
+        if (delimiterPart.includes("][")) {
+            const delimiters = delimiterPart
+                .slice(1, -1)  // Remove outer brackets
+                .split("][")   // Split multiple delimiters
+                .map(d => escapeRegExp(d))
+                .join("|");    // Join as regex pattern
+            delimiter = new RegExp(delimiters);
+        } else {
+            delimiter = new RegExp(escapeRegExp(delimiterPart));
+        }
     }
-    numbers = numbers.split(delimiter).map(Number) //splitting the array based on the delimiter and converting each value to number
-    
+
+    numbers = numbers.split(delimiter).map(Number)
+
     const negativeNumbers = numbers.filter(num => num < 0)
-    if (negativeNumbers.length > 0){
+    if (negativeNumbers.length > 0) {
         throw new Error(`negative numbers not allowed ${negativeNumbers.join(',')}`)
     }
 
@@ -19,5 +31,9 @@ function add(numbers) {
     return sumOfValues
 }
 
+// Helper function to escape special regex characters
+function escapeRegExp(string) {
+    return string.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
 
 module.exports = { add }
