6d6edf460c2804ce60d7f10b6b94ffa0eb54a181
Mohit <mohit94596@gmail.com>
2025-02-18 16:23:08 +0530

    Implement: Custom delimiter supported

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,8 +2,14 @@ function add(numbers) {
     if (numbers === '') {
         return 0
     }
-    const nums = numbers.split(/[\n,]/).map(Number) //splitting the array based on the delimiter and converting each value to number
-    const sumOfValues = nums.reduce((sum, num) => sum + num, 0) //finding the sum of values, works for any amount of numbers
+    let delimiter = /[,\n]/
+    if (numbers.startsWith('//')){
+        const parts = numbers.split('\n')
+        delimiter = parts[0].substring(2)
+        numbers = parts[1]
+    }
+    numbers = numbers.split(delimiter).map(Number) //splitting the array based on the delimiter and converting each value to number
+    const sumOfValues = numbers.reduce((sum, num) => sum + num, 0) //finding the sum of values, works for any amount of numbers
     return sumOfValues
 }
 
